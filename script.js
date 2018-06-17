d5c2dd034e0aa07e66332bdbf3f8022f35519005
chunktooth <om.saetehn@gmail.com>
2018-06-17 15:11:19 -0600

    refactored all

--- script.js
+++ script.js
@@ -1,115 +1,68 @@
-// target all grab-able elements
-var userGuess = document.querySelector('.user-input');
-var guessButton = document.querySelector('.guess');
-var clearButton = document.querySelector('.clear');
-var numberDisplay = document.querySelector('.num');
-var randomRoundOne = Math.floor(Math.random() * 100) + 1;
-var messageDisplay = document.querySelector('.msg');
-var resetButton = document.querySelector('.reset');
+var form = document.querySelector('form');
+var minInput = document.querySelector('.min-input');
+var maxInput = document.querySelector('.max-input');
+var enterButton = document.querySelector('.enter-btn');
+var guessInput = document.querySelector('.guess-input');
+var guessButton = document.querySelector('.guess-btn');
+var clearButton = document.querySelector('.clear-btn');
+var resetButton = document.querySelector('.reset-btn');
+var randomNum;
 
-// check if randomizer functions
-console.log ('random I: ' + randomRoundOne);
-
-// enable ALL buttons once user gives input
-userGuess.addEventListener('click', function() {
-  guessButton.removeAttribute('disabled');
-  clearButton.removeAttribute('disabled');
-  resetButton.removeAttribute('disabled');
-});
-
-// click 'Guess' to grab user's value and display it
-guessButton.addEventListener('click', function(event) {
-  numberDisplay.innerText = userGuess.value;
-  event.preventDefault();
-});
-
-// click 'Guess' to validate user's guess
-guessButton.addEventListener('click', checkRoundOne);
-function checkRoundOne() {
-  var guessRoundOne = parseInt(userGuess.value);
-  if (guessRoundOne === randomRoundOne) {
-    messageDisplay.innerText = 'BOOM!';    
-    var hiddenSection = document.querySelector('.hidden');
-    hiddenSection.classList.remove('hidden');
-    userGuess.value = '';
-  } else if (guessRoundOne < randomRoundOne) {
-    messageDisplay.innerText = 'That is too looow...';
-      if (guessRoundOne < 1) {
-        messageDisplay.innerText = 'No-no. No negative Nancy.';
-      }
-  } else if (guessRoundOne > randomRoundOne) {
-    messageDisplay.innerText = 'That is toooo high!';
-    if (guessRoundOne > 100) {
-      messageDisplay.innerText = "Slow down! 1-100 only";
-    }  
-  } else {
-    messageDisplay.innerText = 'Number only plz!';
-  }
-}
-
-// click 'Clear' to clear input field
-clearButton.addEventListener('click', function() {
-  userGuess.value = '';
-  clearButton.setAttribute('disabled', true);
-  guessButton.setAttribute('disabled', true);
-  resetButton.setAttribute('disabled', true);
-  enterButton.setAttribute('disabled', true);
-});
-
-// click 'Reset' to reset game or reload page
+form.addEventListener('input', activateEnter);
+form.addEventListener('input', activateGuess);
+enterButton.addEventListener('click', submitRange);
+guessButton.addEventListener('click', checkResult);
 resetButton.addEventListener('click', function() {
-  window.location.reload(true);
+  window.location.reload();
 });
 
-// target new elements in Round 2
-var minRange = document.querySelector('.min-input');
-var maxRange = document.querySelector('.max-input');
-var enterButton = document.querySelector('.enter');
-
-// enable 'Enter' button once input min & max value
-minRange.addEventListener('click', activateEnterButton, false);
-maxRange.addEventListener('click', activateEnterButton, false);
-function activateEnterButton () {
-  enterButton.removeAttribute('disabled');
-  
-}
-
-// hit 'Enter' to validate user's guess Round 2
-enterButton.addEventListener('click', checkRoundTwo);
-function checkRoundTwo() {
-  numberDisplay.innerText = '??';
-  messageDisplay.innerText = '';
-  console.log('check message: ' + messageDisplay.innerText);
-  var readyText = document.querySelector('.hidden-ready');
-  readyText.classList.remove('hidden-ready');
-  var guessRoundTwo = parseInt(userGuess.value);
-  var minValue = parseInt(minRange.value);
-  var maxValue = parseInt(maxRange.value);
-  var randomRoundTwo = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
-  if (guessRoundTwo === randomRoundTwo) {
-    messageDisplay.innerText = 'BING BANG BOOM!';    
-  } else if (guessRoundTwo < randomRoundTwo) {
-    messageDisplay.innerText = 'That is too looow...';
-      if (guessRoundTwo < minValue) {
-        messageDisplay.innerText = 'Your mininum number is ' + minValue;
-      }
-  } else if (guessRoundTwo > randomRoundTwo) {
-    messageDisplay.innerText = 'That is toooo high!';
-    if (guessRoundTwo > maxValue) {
-      messageDisplay.innerText = 'Your maximum number is ' + maxValue;
-    }  
+function activateEnter(e) {
+  e.preventDefault();
+  if(minInput.value !== '' && maxInput.value !== '') {
+    enterButton.removeAttribute('disabled');
   } else {
-    messageDisplay.innerText = 'Number only plz!';
+    enterButton.setAttribute('disabled', true);
   }
-  console.log ('random II: ' + randomRoundTwo);
-}
+}; 
 
+function activateGuess(e) {
+  var buttons = [guessButton, clearButton, resetButton];
 
+  e.preventDefault();
+  buttons.forEach(function(btn) {
+    if(guessInput.value !== '') {
+      btn.removeAttribute('disabled');
+    } else {
+      btn.setAttribute('disabled', true);
+    };
+  });
+};
 
+function submitRange(e) {
+  e.preventDefault();
+  var min = parseInt(minInput.value);
+  var max = parseInt(maxInput.value);
+  randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log('randomNum:', randomNum);
+};
 
+function checkResult(e) {  
+  e.preventDefault();
 
+  var guessValue = guessInput.value;
+  var guessDisplay = document.querySelector('.guess-display');
+  var message = document.querySelector('.msg');
 
-
-// FAILED BREAKS:
-// 1) global 'var userValue = userGuess.value'
-// 2) global 'var numberGuess = parseInt(userValue)'
+  console.log(guessValue, randomNum)
+  guessDisplay.innerText = guessValue;
+  
+  if(guessValue == randomNum){
+    message.innerText = 'BOOM!';
+  } else if(guessValue < randomNum) {
+    message.innerText = 'Wayy too low!';
+  } else if(guessValue > randomNum) {
+    message.innerText = 'Too high! Slow down killer';
+  } else if(isNaN(guessValue)) {
+    message.innerText = 'Number only plz!';
+  }
+};
